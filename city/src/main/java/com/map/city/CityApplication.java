52890b74086cf4a0898ac9cdad53a44da15204f3
srishith552-create <srishith552@gmail.com>
2026-07-11 20:49:16 +0530

    Initial commit for fantasyMap repository

---
+++ city/src/main/java/com/map/city/CityApplication.java
@@ -0,0 +1,12 @@
+// ...existing code...
+package com.map.city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CityApplication.class, args);
+    }
+} //game of thrones map with near identical feautures of a map 
